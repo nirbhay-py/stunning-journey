2d356b7edc9a2dbe0e61e7172382c3a6fd75602a
nirbhay-py <nnarang1@jpischool.com>
2020-03-19 07:59:58 +0530

    Added map-2

---
+++ EconomicsProject/map2VC.swift
@@ -0,0 +1,30 @@
+//
+//  map2VC.swift
+//  EconomicsProject
+//
+//  Created by Nirbhay Singh on 19/03/20.
+//  Copyright © 2020 Valley. All rights reserved.
+//
+
+import UIKit
+
+class map2VC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
